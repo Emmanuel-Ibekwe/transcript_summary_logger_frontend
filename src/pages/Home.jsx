8ca3234724e28d5985b0f07239d4bc93344dd11a
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-21 17:46:18 +0100

    feat: update the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,61 +3,79 @@ import { useEffect, useState } from "react";
 import MainContent from "../components/Maincontent";
 import Pagination from "../components/Pagination";
 import Transcript from "../components/Transcript";
+import Spinner from "../components/Spinner";
+import ToggleButton from "../components/ToggleButton";
 
 const ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODRhYTdhMDNlYTBkMzJiM2Y1OTY3M2UiLCJlbWFpbCI6ImliZWt3ZWVtbWFudWVsMDA3QGdtYWlsLmNvbSIsImlhdCI6MTc1MDQxODMxOCwiZXhwIjoxNzUwNTA0NzE4fQ.ETbno1Djt15ackMR2KgO87iFX9Ovdh7Tx5sYGdAC9jw";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODRhYTdhMDNlYTBkMzJiM2Y1OTY3M2UiLCJlbWFpbCI6ImliZWt3ZWVtbWFudWVsMDA3QGdtYWlsLmNvbSIsImlhdCI6MTc1MDUwNTE5MiwiZXhwIjoxNzUwNTkxNTkyfQ.nne0ReiCsDJQ-cqy3gnRydmmSPQPLxfutNyUkN_T2es";
 const LIMIT = 10;
 const Home = () => {
   const [transcripts, setTranscripts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalCount, setTotalCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [toggle, setToggle] = useState(false);
+  const [error, setError] = useState("");
 
-  const fetchTranscripts = async (page) => {
-    try {
-      const response = await axios.get(
-        `https://transcript-summarizer-1.onrender.com/api/v1/transcripts?page=${page}`,
-        {
-          headers: {
-            Authorization: `Bearer ${ACCESS_TOKEN}`,
-          },
-        }
-      );
-      console.log(response.data);
-
-      setTranscripts(response.data.transcripts);
-      setTotalCount(response.data.totalTranscripts);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+  // page,
+  // limit,
+  // toggle,
+  // accessToken,
+  // setLoading,
+  // setTranscripts,
+  // setTotalCount,
+  // setError
 
   useEffect(() => {
-    fetchTranscripts(currentPage);
-    console.log("Hello");
-  }, [currentPage]);
+    async function setStates() {
+      const { loadingState, totalCountState, transcriptsState, errorState } =
+        await fetchTranscripts(currentPage, LIMIT, false, ACCESS_TOKEN);
+      setLoading(loadingState);
+      setTotalCount(totalCountState);
+      setTranscript(transcriptsState[0]);
+      setError(errorState);
+      console.log("Hello");
+    }
+    setStates();
+  }, [currentPage, toggle]);
 
   return (
     <MainContent>
       <div>
-        <ul className="mx-auto max-w-3xl w-[90%] bg-white rounded-md">
-          {transcripts.map((el) => (
-            <Transcript
-              key={el.videoId}
-              title={el.title}
-              url={el.url}
-              transcript={el.transcript}
-              summary={el.summary}
-              newsChannel={el.newsChannel}
+        <div className="mx-auto max-w-3xl w-[90%] bg-white rounded-md p-5">
+          <div className="flex w-full justify-end items-center space-x-2 text-sm">
+            <span>Sort transcripts with summaries</span>
+            <ToggleButton
+              toggle={toggle}
+              onToggle={() => setToggle((prev) => !prev)}
             />
-          ))}
-        </ul>
-        <Pagination
-          onPageChange={(page) => setCurrentPage(page)}
-          totalCount={totalCount}
-          siblingCount={1}
-          currentPage={currentPage}
-          pageSize={LIMIT}
-        />
+          </div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div className="w-full">
+              {transcripts.map((el) => (
+                <Transcript
+                  videoId={el.videoId}
+                  title={el.title}
+                  url={el.url}
+                  transcript={el.transcript}
+                  summary={el.summary}
+                  newsChannel={el.newsChannel}
+                  removeActionSection={false}
+                />
+              ))}
+
+              <Pagination
+                onPageChange={(page) => setCurrentPage(page)}
+                totalCount={totalCount}
+                siblingCount={1}
+                currentPage={currentPage}
+                pageSize={LIMIT}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </MainContent>
   );
