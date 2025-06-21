c01af211df0c51b2a01b1065e01a9bd0ae59c664
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-21 17:44:31 +0100

    feat: update the Transcript component

--- src/components/Transcript.jsx
+++ src/components/Transcript.jsx
@@ -1,33 +1,63 @@
 import ReadMore from "./ReadMore";
+import Button from "./Button";
+import { capitalizeTitle } from "../utils/formatting";
 
-const Transcript = ({ key, title, url, transcript, summary, newsChannel }) => {
+const MIN_SUMMARY_LENGTH = 200;
+
+const Transcript = ({
+  videoId,
+  title,
+  url,
+  transcript,
+  summary,
+  newsChannel,
+  removeActionSection,
+}) => {
   return (
-    <li
-      className="max-w-[80%] mx-auto p-4 border-b-2 border-[#E0E0E0]"
-      key={key}
+    <div
+      className="max-w-[80%] mx-auto p-4 border-b-2 border-[#E0E0E0] "
+      key={videoId}
     >
-      <div className="flex justify-end w-full ">
-        <div className="flex justify-center h-8 w-8 rounded-full text-[#333] text-lg items-center border border-[#8b6d5c]">
-          &rarr;
-        </div>
-      </div>
-      <div>{title}</div>
-      <div className="text-sm text-blue-500">{url}</div>
-      <div>{newsChannel}</div>
-      <ReadMore text={transcript} maxChars={100} />
-      <div className="flex w-1/3 justify-between items-center">
-        <p>Has Summary</p>
-        <div
-          className={`flex w-8 h-8 rounded-full items-center justify-center border border-[#8b6d5c] ${
-            summary.length >= 100
-              ? "text-white bg-[#8b6d5c]"
-              : "text-[#8b6d5c] bg-white"
-          } `}
+      <div className="font-robotoSlab font-bold text-lg">
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-slate-500"
         >
-          &#10003;
-        </div>
+          {capitalizeTitle(title)}
+        </a>
       </div>
-    </li>
+
+      <div className="text-sm text-[#8b6d5c]">
+        News channel:{" "}
+        <span className="text-[#bca79a">{`${newsChannel}`.toUpperCase()}</span>
+      </div>
+      <ReadMore text={transcript} maxChars={300} />
+      {!removeActionSection && (
+        <div className="flex w-1/3 justify-between items-center">
+          <p className="text-slate-500 font-semibold">Has Summary</p>
+          <div
+            className={`flex w-5 h-5 rounded-full items-center justify-center border-2 border-[#8b6d5c] ${
+              summary.length >= MIN_SUMMARY_LENGTH
+                ? "text-white text-xs font-semibold bg-[#8b6d5c]"
+                : "text-[#8b6d5c] bg-white"
+            } `}
+          >
+            {summary.length >= MIN_SUMMARY_LENGTH && <span>&#10003;</span>}
+          </div>
+        </div>
+      )}
+      {!removeActionSection && (
+        <div className="w-full flex justify-end items-center">
+          <Button onClick={() => {}}>
+            {summary.length >= MIN_SUMMARY_LENGTH
+              ? "View summary"
+              : "Enter summary"}
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 
