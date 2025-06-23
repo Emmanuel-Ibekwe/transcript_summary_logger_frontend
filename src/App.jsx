5eee0619fcb00ab8f4d2024fda931cccc9f8c03f
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-23 17:31:46 +0100

    feat: update the App component logic to include the routing capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./App.css";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Navigation from "./components/Navigation";
+import AddSummary from "./pages/AddSummary";
+import Signup from "./pages/Signup";
 import ForgetPassword from "./pages/ForgetPassword";
 import ResetPassword from "./pages/ResetPassword";
 
 function App() {
+  const { accessToken } = useSelector((state) => state.user);
+
   return (
-    <>
-      <Navigation />
-      <ResetPassword />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={accessToken ? <Home /> : <Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={accessToken ? <Home /> : <Login />} />
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route path="reset-password" element={<ResetPassword />} />
+          <Route
+            path="transcripts/:videoId"
+            element={accessToken ? <AddSummary /> : <Login />}
+          />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
