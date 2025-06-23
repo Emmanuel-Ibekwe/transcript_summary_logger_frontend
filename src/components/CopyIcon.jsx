c5b8629c9b1e5f8e0278d09d72c23a15968ba785
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-23 17:25:29 +0100

    feat: create the CopyIcon component

---
+++ src/components/CopyIcon.jsx
@@ -0,0 +1,27 @@
+const CopyIcon = ({
+  className = "",
+  width = 24,
+  height = 24,
+  fill = "currentColor",
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 115.77 122.88"
+      xmlns="http://www.w3.org/2000/svg"
+      fill={fill}
+      className={className}
+    >
+      <g>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34..."
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default CopyIcon;
