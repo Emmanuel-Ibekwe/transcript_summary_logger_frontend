d7b7851334348e23bc3a32a13c41f41f6c036810
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-23 17:29:54 +0100

    feat: create the persistor and store of the redux toolkit

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import userReducer from "./userSlice";
+import passwordReducer from "./passwordSlice";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  password: passwordReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
