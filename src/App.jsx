5eb38e1bc464a85fa3eca36ecc66d5db07124e73
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-24 17:48:45 +0100

    feat: add code to redirect to /transcript route when / is entered. Also add a new route to /refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Root from "./pages/Root";
@@ -8,6 +13,7 @@ import AddSummary from "./pages/AddSummary";
 import Signup from "./pages/Signup";
 import ForgetPassword from "./pages/ForgetPassword";
 import ResetPassword from "./pages/ResetPassword";
+import Refresh from "./pages/Refresh";
 
 function App() {
   const { accessToken } = useSelector((state) => state.user);
@@ -16,11 +22,21 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Root />}>
-          <Route index element={accessToken ? <Home /> : <Login />} />
+          <Route
+            index
+            element={
+              accessToken ? <Navigate to="/transcripts" replace /> : <Login />
+            }
+          />
+          <Route
+            path="transcripts"
+            element={accessToken ? <Home /> : <Login />}
+          />
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={accessToken ? <Home /> : <Login />} />
           <Route path="forget-password" element={<ForgetPassword />} />
           <Route path="reset-password" element={<ResetPassword />} />
+          <Route path="refresh" element={<Refresh />} />
           <Route
             path="transcripts/:videoId"
             element={accessToken ? <AddSummary /> : <Login />}
